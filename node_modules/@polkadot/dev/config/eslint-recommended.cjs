696790ed16fa173981e533d166acca8470c1a73d
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2024-03-12 10:08:06 +0700

    chore: removed node_modules from tracking

--- node_modules/@polkadot/dev/config/eslint-recommended.cjs
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2023 @polkadot/dev authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-const configJs = require('@eslint/js');
-
-module.exports = configJs.configs.recommended;
