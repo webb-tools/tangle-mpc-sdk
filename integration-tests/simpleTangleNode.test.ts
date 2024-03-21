156961331f53357c9ca7faf622d41088de5246bf
salman01zp <pathansalman555@gmail.com>
2024-03-21 22:53:03 +0700

    yarn format

--- integration-tests/simpleTangleNode.test.ts
+++ integration-tests/simpleTangleNode.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/order */
 
-import '@webb-tools/tangle-substrate-types';
+import "@webb-tools/tangle-substrate-types";
 
 import isCi from "is-ci";
 import path from "path";
@@ -16,9 +16,7 @@ describe("Substrate SignatureBridge Governor Update", function () {
       ? { mode: "docker", forcePullImage: false }
       : {
           mode: "host",
-          nodePath: path.resolve(
-            "../../tangle/target/release/tangle",
-          ),
+          nodePath: path.resolve("../../tangle/target/release/tangle"),
         };
 
     // start tangle nodes.
@@ -29,14 +27,12 @@ describe("Substrate SignatureBridge Governor Update", function () {
       ports: "auto",
       enableLogging: false,
     });
-
-    
   });
 
   it("Start tangle node", async () => {
     const api = await aliceNode.api();
     await api.isReady;
-    
+
     const time = await api.query.timestamp.now();
     console.log(`Current time: ${time}`);
 
